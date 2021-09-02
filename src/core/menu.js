7b07a530a60823506fddfc80597c96fca5418bc1
Gana016 <jayam.ganapathi12@gmail.com>
2021-09-02 20:09:51 +0530

    navbar update

--- src/core/menu.js
+++ src/core/menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import { Navbar, Nav, Container} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const currentTab = (history, path) => {
   if (history.location.pathname === path) {
@@ -13,26 +13,26 @@ const currentTab = (history, path) => {
 const Menu = ({ history }) => {
   return (
     <>
-  <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Amrita Alumni Portal</Navbar.Brand>
-    <Nav className="me-auto">
-      {/* <Nav.Link href="#home">Home</Nav.Link> */}
-      <Nav.Link href="#blogs">Blogs</Nav.Link>
-      <Nav.Link href="#chat">Messenger</Nav.Link>
-      <Nav.Link href="#gallary">Gallary</Nav.Link>
-      <Nav.Link href="#more">More..</Nav.Link>
-    </Nav>
-    <Navbar.Collapse className="justify-content-end">
-      <Nav.Link href="#register">REGISTER</Nav.Link>
-      <Nav.Link href="#login">LOGIN</Nav.Link>
-    </Navbar.Collapse>
-    {/* <Navbar.Collapse className="justify-content-end">
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Amrita Alumni Portal</Navbar.Brand>
+          <Nav className='me-auto'>
+            {/* <Nav.Link href="#home">Home</Nav.Link> */}
+            <Nav.Link href='#blogs'>Blogs</Nav.Link>
+            <Nav.Link href='#chat'>Messenger</Nav.Link>
+            <Nav.Link href='#gallary'>Gallary</Nav.Link>
+            <Nav.Link href='#more'>More..</Nav.Link>
+          </Nav>
+          <Navbar.Collapse className='justify-content-end'>
+            <Nav.Link href='/signup'>REGISTER</Nav.Link>
+            <Nav.Link href='/signin'>LOGIN</Nav.Link>
+          </Navbar.Collapse>
+          {/* <Navbar.Collapse className="justify-content-end">
       <Nav.Link href="#home">LOGOUT</Nav.Link>
     </Navbar.Collapse> */}
-    </Container>
-  </Navbar>
-</>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
