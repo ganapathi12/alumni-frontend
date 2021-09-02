83a5146202dd2c585a9c50df772ae7eaa50852af
Gana016 <jayam.ganapathi12@gmail.com>
2021-09-02 20:10:56 +0530

    nav

--- src/core/menu.js
+++ src/core/menu.js
@@ -15,7 +15,7 @@ const Menu = ({ history }) => {
     <>
       <Navbar bg='dark' variant='dark'>
         <Container>
-          <Navbar.Brand href='#home'>Amrita Alumni Portal</Navbar.Brand>
+          <Navbar.Brand href='/'>Amrita Alumni Portal</Navbar.Brand>
           <Nav className='me-auto'>
             {/* <Nav.Link href="#home">Home</Nav.Link> */}
             <Nav.Link href='#blogs'>Blogs</Nav.Link>
